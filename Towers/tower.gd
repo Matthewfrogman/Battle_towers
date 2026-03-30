9f431b123d6c03027110f88e729a13a388680dbe
thetumbleweed <logan.koegler@gmail.com>
2026-03-30 13:53:24 -0400

    Working angled bullets 2
    the angled bullets are NOW actually working

--- Towers/tower.gd
+++ Towers/tower.gd
@@ -11,15 +11,13 @@ var bullet_scene: PackedScene = preload("res://Towers/Bullets/bullet.tscn")
 #the time it takes to shoot
 @export var cooldown: float = 5.0
 @export var attack: int = 5
-<<<<<<< Updated upstream
-=======
 #num of bullets. this number should ALWAYS be odd so it looks good.
 @export var projectiles: int = 5
 
->>>>>>> Stashed changes
 var canshoot = false
 #either hover or placed. If its hovering itll follow the mouse, otherwise itll shoot
 var mode = "hover"
+
 func _ready() -> void:
 	timer.wait_time = cooldown
 	mode = "hover"
@@ -33,63 +31,39 @@ func _process(delta: float) -> void:
 			print(position)
 	if mode == "placed":
 		#get the difference between x and y coords with the closest enemy
-<<<<<<< Updated upstream
 		#gives us an angle in radians!!
 		var angle: float = 0.0
 		if canshoot == true:
-			shoot(delta, 10, angle, "straight")
-=======
-		#gives an angle in radians!!
-		var angle: float
-		if canshoot == true:
-			shoot(50, angle, "angled", 5)
+			shoot(delta, 50, angle, "angled", 5)
 
 
->>>>>>> Stashed changes
 #PLANS FOR THIS:
-#shoot(eventually some other name) controls the direction and amount
-#of each bullet, and also the angles
-#whereas shoot2(eventually some other name) actually instantiates the bullets
-func shoot(speed: int, angle: float, angle_mode: String, bnum: int):
-	#straight will shoot the bullets parallel, and angled wont
-	
+#shoot controls the direction and amount of each bullet, and also the angles
+#whereas bulletShoot actually instantiates the bullets and stuff
+func shoot(delta: float, speed: int, angle: float, angle_mode: String, bnum: int):
 	if angle_mode == "straight":
-<<<<<<< Updated upstream
-		#same vector
-		var adj = speed * cos(angle)
-		var opp = speed * sin(angle)
-		for shots in 5:
-=======
-		for shots in 1:
->>>>>>> Stashed changes
-			print("blast")
-			bulletShoot(Vector2(speed*cos(angle), speed*sin(angle)), position)
-		#change the mode AGAIN based on if its even or odd
+		#doesn't do anything with multiple projectiles rn
+		bulletShoot(Vector2(speed*cos(angle), speed*sin(angle)), position)
+		
 	if angle_mode == "angled":
-<<<<<<< Updated upstream
-		pass
-func shoot2(move: Vector2, pos: Vector2):
-=======
 		bulletShoot(Vector2(speed*cos(angle), speed*sin(angle)), position)
 		var aIncrement = 0
 		for i in bnum-1:
 			#error for integer division
 			if i > (bnum-1)/2-1:
 				aIncrement = -0.1*(i+1-((bnum-1)/2))
-				print(aIncrement)
 			else:
 				aIncrement = 0.1*(i+1)
-				print(aIncrement)
 			bulletShoot(Vector2(speed*cos(angle+aIncrement), speed*sin(angle+aIncrement)), position)
-			
+		
 
 func bulletShoot(move: Vector2, pos: Vector2):
->>>>>>> Stashed changes
 	var bullet = bullet_scene.instantiate()
 	add_child(bullet)
 	bullet.global_position = pos
 	bullet.damage = attack
 	bullet.move = move
 	canshoot = false
+	
 func _timer_timeout() -> void:
 	canshoot = true
