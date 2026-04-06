7e43a0f26a4d1713bd309219d7267475d7a3e3aa
thetumbleweed <logan.koegler@gmail.com>
2026-04-06 12:16:36 -0400

    Moved the basic_tower stuff to tower

--- Towers/tower.gd
+++ Towers/tower.gd
@@ -42,6 +42,10 @@ func _process(delta: float) -> void:
 			mode = "placed"
 			
 	if mode == "placed":
+		cannon_scene.rotation = angle
+		#an extra option if needed
+		#cannon_scene.look_at(lookingat)
+		
 		#get the difference between x and y coords with the closest enemy
 		#gives us an angle in radians!!
 		for enemy in range_scene.get_overlapping_bodies():
@@ -50,6 +54,7 @@ func _process(delta: float) -> void:
 			var hyp = (adj**2 + opp**2)**0.5
 			angle = atan2(-opp, -adj)
 			lookingat = enemy.global_position
+		
 		
 		if len(range_scene.get_overlapping_bodies()) > 0: 
 			sees_enemy = true
