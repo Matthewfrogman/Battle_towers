48269ea1166566b60904d59e54a34f2ad0227527
kailoish1gh <michealgreathouse69@gmail.com>
2026-04-13 12:45:07 -0400

    enemy drops money

--- scenes/enemy_base.gd
+++ scenes/enemy_base.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 class_name Enemy
-
 @export var speed = 35
 @export var hp = 100
 @export var direction = 1
 @export var dmg_to_player = 50
-
 # sees the progress of the enemy across the track
 var progress = 0
 
@@ -14,19 +12,16 @@ func _ready():
 
 func _process(delta):
 	progress += 1 * delta
-
-	if direction == 1:#Right
-		position.x += delta*speed
-	elif direction == 2:#Up
-		position.y -= delta*speed
-	elif direction == 3:#Left
-		position.x -= delta*speed
-	elif direction == 4:#Down
-		position.y += delta*speed
-
+	if direction == 1: # Right
+		position.x += delta * speed
+	elif direction == 2: # Up
+		position.y -= delta * speed
+	elif direction == 3: # Left
+		position.x -= delta * speed
+	elif direction == 4: # Down
+		position.y += delta * speed
 	if hp <= 100:
 		modulate = Color(hp / 100.0, hp / 100.0, hp / 100.0)
-
 	if hp <= 0:
 		die()
 
@@ -34,8 +29,8 @@ func lose_hp(dmg):
 	hp -= dmg
 
 func die():
-	# Add money to UI
-	var ui = get_tree().root.get_node_or_null("EnemySpawner/@CanvasLayer@10")
-	if ui and ui.has_method("add_money"):
-		ui.add_money(20)
+	# Find the shop UI via group (reliable regardless of node name changes)
+	var ui_nodes = get_tree().get_nodes_in_group("shop_ui")
+	if ui_nodes.size() > 0:
+		ui_nodes[0].add_money(20)
 	queue_free()
