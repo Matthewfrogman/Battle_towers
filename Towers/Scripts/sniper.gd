fd03bd9cbd739401ff4d2df6b67ce33c7e9bce01
thetumbleweed <logan.koegler@gmail.com>
2026-04-14 00:08:15 -0400

    Muzzle

--- Towers/Scripts/sniper.gd
+++ Towers/Scripts/sniper.gd
@@ -1,9 +1,14 @@
 extends Tower
 
+@onready var muzzle_sprite = get_node("Cannon/muzzleflash")
+
 #keep the projectiles, bullet speed, pierce, and bullet scene 0 or empty
 func _process(_delta: float) -> void:
 	super(_delta)
 	if canshoot and sees_enemy:
 		if enemies["first"] is Array:
+			muzzle_sprite.visible = true
 			enemies["first"][0].hp -= attack
 			canshoot = false
+			await get_tree().create_timer(0.1).timeout
+			muzzle_sprite.visible = false
