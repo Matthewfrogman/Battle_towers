977984b959618985f41b4c1a610b3298312daf38
kailoish1gh <michealgreathouse69@gmail.com>
2026-04-15 13:10:57 -0400

    quickpush

--- EnemySpawner/enemy_spawner.gd
+++ EnemySpawner/enemy_spawner.gd
@@ -15,6 +15,14 @@ const PATH_BOSS3   = "res://enemies/Boss_3.tscn"
 
 var spawn_interval: float = 0.4
 
+var button_size := Vector2(64, 64)
+var button_color := Color(0.18, 0.72, 0.22)
+var outline_normal := Color(0.05, 0.05, 0.05)
+var outline_auto := Color(1.0, 0.85, 0.0)
+var outline_width := 5.0
+var corner_radius := 10.0
+var button_margin := 16
+
 var current_wave: int = 0
 var wave: int = 1
 
@@ -27,12 +35,24 @@ var _spawn_timer: float = 0.0
 var _spawning: bool = false
 var _auto_mode: bool = false
 
+var _btn: Button
+var _canvas: CanvasLayer
+var _style_normal: StyleBoxFlat
+var _style_hover: StyleBoxFlat
+var _style_press: StyleBoxFlat
+
+var _wave_click_count: int = 0
+
 func _ready() -> void:
 	randomize()
 	_try_load_scenes()
+
 	_spawner = get_node_or_null(spawner_path)
 	if _spawner:
 		_spawner_start_pos = _spawner.global_position
+
+	_build_button()
+	get_tree().get_root().size_changed.connect(_reposition_button)
 
 func _try_load_scenes() -> void:
 	var paths := {
@@ -51,17 +71,82 @@ func _try_load_scenes() -> void:
 		else:
 			_scenes[key] = null
 
+func _build_button() -> void:
+	_btn = Button.new()
+	_btn.custom_minimum_size = button_size
+	_btn.size = button_size
+	_btn.text = ">"
+
+	_style_normal = _make_style(outline_normal)
+	_style_hover = _make_style(outline_normal, 0.12)
+	_style_press = _make_style(outline_normal, -0.10)
+
+	_btn.add_theme_stylebox_override("normal", _style_normal)
+	_btn.add_theme_stylebox_override("hover", _style_hover)
+	_btn.add_theme_stylebox_override("pressed", _style_press)
+
+	_btn.add_theme_color_override("font_color", Color.WHITE)
+	_btn.add_theme_font_size_override("font_size", 28)
+
+	_btn.pressed.connect(_on_button_pressed)
+
+	_canvas = CanvasLayer.new()
+	_canvas.layer = 10
+	add_child(_canvas)
+	_canvas.add_child(_btn)
+
+	_reposition_button()
+
+func _make_style(outline_col: Color, brightness_offset: float = 0.0) -> StyleBoxFlat:
+	var s := StyleBoxFlat.new()
+	s.bg_color = button_color.lightened(brightness_offset)
+	s.border_color = outline_col
+	s.set_border_width_all(int(outline_width))
+	s.set_corner_radius_all(int(corner_radius))
+	return s
+
+func _reposition_button() -> void:
+	var vp := get_tree().get_root().get_visible_rect().size
+	_btn.position = Vector2(
+		vp.x - button_size.x - button_margin,
+		vp.y - button_size.y - button_margin
+	)
+
+func _on_button_pressed() -> void:
+	_wave_click_count += 1
+
+	if _auto_mode:
+		_auto_mode = false
+		_set_outline(outline_normal)
+		_wave_click_count = 0
+		return
+
+	if _wave_click_count >= 2:
+		_auto_mode = true
+		_set_outline(outline_auto)
+
+	if not _spawning:
+		_launch_current_wave()
+
+func _process(delta: float) -> void:
+	_handle_spawning(delta)
+
+func _set_outline(col: Color) -> void:
+	_style_normal.border_color = col
+	_style_hover.border_color = col
+	_style_press.border_color = col
+
 const WAVES: Array = [
 	[["basic", 20]],
-	[["basic", 30]],
-	[["basic", 25], ["speeder", 10]],
-	[["basic", 30], ["speeder", 15]],
-	[["basic", 20], ["speeder", 25]],
-	[["basic", 15], ["tank", 10]],
-	[["basic", 20], ["tank", 15]],
-	[["speeder", 40]],
-	[["tank", 25]],
-	[["basic", 30], ["speeder", 30]],
+	[["basic", 35]],
+	[["basic", 30], ["speeder", 5]],
+	[["basic", 35], ["speeder", 15]],
+	[["basic", 10], ["speeder", 25]],
+	[["basic", 15], ["speeder", 10], ["tank", 10], ["camo", 10]],
+	[["basic", 20], ["speeder", 20], ["tank", 15], ["camo", 15]],
+	[["basic", 15], ["speeder", 25], ["tank", 20]],
+	[["tank", 30]],
+	[["basic", 25], ["speeder", 45], ["tank", 5]],
 
 	[["basic", 20], ["tank", 20], ["camo", 10]],
 	[["speeder", 35], ["camo", 15]],
@@ -113,9 +198,6 @@ const WAVES: Array = [
 	[["boss", 1]]
 ]
 
-func _process(delta: float) -> void:
-	_handle_spawning(delta)
-
 func _launch_current_wave() -> void:
 	start_wave(current_wave)
 
@@ -161,6 +243,8 @@ func _on_wave_finished() -> void:
 	if current_wave >= WAVES.size():
 		current_wave = 0
 		wave = 1
+		_auto_mode = false
+		_set_outline(outline_normal)
 
 func _spawn_next() -> void:
 	var type: String = _spawn_queue.pop_front()
