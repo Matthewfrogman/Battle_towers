90d24c8a67b1bd1bf6dfa23aaabbf920ebbd477a
99zeehC <cheesbro798@gmail.com>
2026-04-15 13:54:36 -0400

    Wave john

--- scenes/wave_counter.gd
+++ scenes/wave_counter.gd
@@ -9,4 +9,4 @@ func _process(delta: float) -> void:
 	update_wave_label()
 
 func update_wave_label():
-	text = "Wave: " + str(get_parent().get_node("EnemySpawner").wave)
+	text = "Wave: " + str(get_parent().get_node("EnemySpawner").current_wave)
