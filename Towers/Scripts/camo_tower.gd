4d0ef1a3fb9a435c826c6cc0cd952993eaf9d5b2
thetumbleweed <logan.koegler@gmail.com>
2026-04-20 12:07:47 -0400

    Small Fixes

--- Towers/Scripts/camo_tower.gd
+++ Towers/Scripts/camo_tower.gd
@@ -1,20 +1,5 @@
 extends Tower
 
-@onready var muzzle_sprite = get_node("Cannon/muzzleflash")
-
-#keep the projectiles, bullet speed, pierce, and bullet scene 0 or empty
-func _process(_delta: float) -> void:
-	super(_delta)
-	if canshoot and sees_enemy:
-		if enemies["first"] is Array and (
-			not enemies["first"][0].camo or enemies["first"][0].camo and sees_camo):
-			muzzle_sprite.visible = true
-			enemies["first"][0].hp -= attack
-			upg_attack(enemies["first"][0])
-			canshoot = false
-			await get_tree().create_timer(0.1).timeout
-			muzzle_sprite.visible = false
-
 func upgrade(upg_path: int):
 	#for all the simple upgrades, and actually upgrades the tower
 	if path[upg_path-1] == 2:
