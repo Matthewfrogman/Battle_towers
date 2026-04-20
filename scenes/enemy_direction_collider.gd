70cee9c800e68b43361a65562d86fcfd96d61f97
thetumbleweed <logan.koegler@gmail.com>
2026-04-20 14:08:05 -0400

    More debloating

--- scenes/enemy_direction_collider.gd
+++ scenes/enemy_direction_collider.gd
@@ -2,12 +2,6 @@ extends Area2D
 
 @export var send_direction = 1
 
-func _ready():
-	pass
-
-func _process(delta):
-	pass
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("lose_hp"):
 		body.direction = send_direction
